227b9d93d1d3fd9a339f62b84cacaa9c8942f033
Vitor Batista de Souza <vitor-bs@hotmail.com>
2022-11-01 13:36:44 -0300

    Corrigido bugs

--- Source/Tainted_renaming/Tainted_renaming.cs
+++ Source/Tainted_renaming/Tainted_renaming.cs
@@ -39,7 +39,9 @@ namespace Tainted_renaming
             {
                 text = GenLabel.ThingLabel(t.def, t.Stuff, 1);
             }
-            text = GenLabel.LabelExtras(t, stackCount, includeHp, includeQuality);
+
+            text = LabelTainted(t) + text + LabelQuality(t, stackCount, includeHp, includeQuality);
+
             if (stackCount > 1)
             {
                 text = text + " x" + stackCount.ToStringCached();
@@ -47,28 +49,13 @@ namespace Tainted_renaming
             __result = text;
             return;
         }
-    }
 
-    [HarmonyPatch(typeof(GenLabel), "LabelExtras", new Type[] { typeof(Thing), typeof(int), typeof(bool), typeof(bool) })]
-    class LabelExtrasClass
-    {
-
-        [HarmonyPostfix]
-        private static void LabelExtras(Thing t, int stackCount, bool includeHp, bool includeQuality, ref string __result)
+        private static string LabelTainted(Thing t)
         {
             Apparel apparel = t as Apparel;
-            bool earlyOut = apparel == null;
-            if(earlyOut == true || apparel.WornByCorpse == false)
-            {
-                return;
-            }
 
-            string text = ""; 
-            QualityCategory cat;
-            bool hasQuality = t.TryGetQuality(out cat);
-            int hitPoints = t.HitPoints;
-            int maxHitPoints = t.MaxHitPoints;
-            bool isDamaged = t.def.useHitPoints && hitPoints < maxHitPoints && t.def.stackLimit == 1 && includeHp;
+            string text = "";
+
             bool isTainted = apparel != null && apparel.WornByCorpse;
 
             if (isTainted)
@@ -77,9 +64,20 @@ namespace Tainted_renaming
                 text += " ";
             }
 
-            text += GenLabel.ThingLabel(t.def, t.Stuff, 1);
+            return text;
+        }
+        private static string LabelQuality(Thing t, int stackCount, bool includeHp, bool includeQuality)
+        {
+            Apparel apparel = t as Apparel;
 
-            if (hasQuality || isDamaged || isTainted)
+            string text = "";
+            QualityCategory cat;
+            bool hasQuality = t.TryGetQuality(out cat);
+            int hitPoints = t.HitPoints;
+            int maxHitPoints = t.MaxHitPoints;
+            bool isDamaged = t.def.useHitPoints && hitPoints < maxHitPoints && t.def.stackLimit == 1 && includeHp;
+
+            if (hasQuality || isDamaged)
             {
                 text += " (";
 
@@ -97,8 +95,60 @@ namespace Tainted_renaming
                 }
                 text += ")";
             }
-            __result = text;
-            return;
+            return text;
         }
     }
+
+    //[HarmonyPatch(typeof(GenLabel), "LabelExtras", new Type[] { typeof(Thing), typeof(int), typeof(bool), typeof(bool) })]
+    //class LabelExtrasClass
+    //{
+
+    //    [HarmonyPostfix]
+    //    private static void LabelExtras(Thing t, int stackCount, bool includeHp, bool includeQuality, ref string __result)
+    //    {
+    //        Apparel apparel = t as Apparel;
+    //        bool earlyOut = apparel == null;
+    //        //if(earlyOut == true || apparel.WornByCorpse == false)
+    //        //{
+    //        //    return;
+    //        //}
+
+    //        string text = ""; 
+    //        QualityCategory cat;
+    //        bool hasQuality = t.TryGetQuality(out cat);
+    //        int hitPoints = t.HitPoints;
+    //        int maxHitPoints = t.MaxHitPoints;
+    //        bool isDamaged = t.def.useHitPoints && hitPoints < maxHitPoints && t.def.stackLimit == 1 && includeHp;
+    //        bool isTainted = apparel != null && apparel.WornByCorpse;
+
+    //        if (isTainted)
+    //        {
+    //            text += "WornByCorpseChar".Translate();
+    //            text += " ";
+    //        }
+
+    //        //text += GenLabel.ThingLabel(t.def, t.Stuff, 1);
+
+    //        if (hasQuality || isDamaged || isTainted)
+    //        {
+    //            text += "(";
+
+    //            if (hasQuality)
+    //            {
+    //                text += cat.GetLabel();
+    //            }
+    //            if (isDamaged)
+    //            {
+    //                if (hasQuality)
+    //                {
+    //                    text += " ";
+    //                }
+    //                text += ((float)hitPoints / (float)maxHitPoints).ToStringPercent();
+    //            }
+    //            text += ") ";
+    //        }
+    //        __result = text;
+    //        return;
+    //    }
+    //}
 }
